be2bd33ea0f4aadde4ee5cc2e4bc64e11cf7d57a
ShrikantJawla <shrikantjawla@gmail.com>
2022-08-06 19:47:57 +0530

    added movieDatase website-app

--- index.js
+++ index.js
@@ -50,10 +50,6 @@ function appendDataInSeachBox(res) {
 		let title = createEl("p");
 		title.innerText = ele.Title;
 		title.style.color = "brown";
-		title.style.cursor = "pointer";
-		title.addEventListener("click", () => {
-			open(ele);
-		});
 
 		let year = createEl("p");
 		year.innerText = ele.Year;
@@ -64,6 +60,11 @@ function appendDataInSeachBox(res) {
 		divText.append(title, year, type);
 		div.append(poster, divText);
 		box.append(div);
+
+		div.style.cursor = "pointer";
+		div.addEventListener("click", () => {
+			open(ele);
+		});
 	});
 }
 
@@ -90,5 +91,74 @@ function escKey() {
 	if (event.key === "Escape") {
 		let box = getEl("box");
 		box.classList.remove("visible");
+	}
+}
+
+//First Cruisor----------------------------------------------------------------------------
+
+const api = "api_key=8a88403617c4c982fe33092da09422d0";
+const baseUrl = "https://api.themoviedb.org/3";
+const imagUrl = "https://image.tmdb.org/t/p/w500";
+let data1;
+async function main1() {
+	try {
+		let res = await fetch(
+			`${baseUrl}/discover/movie?primary_release_date.gte=2022-01-01&primary_release_date.lte=2022-07-22&${api}`
+		);
+		data1 = await res.json();
+		if (data1.results !== undefined) {
+			append2(data1.results);
+		}
+		// console.log(data1.results);
+	} catch (e) {
+		console.log(e);
+	}
+}
+
+main1();
+
+function append2(data) {
+	let box = getEl("fCruisor");
+	box.innerHTML = null;
+
+	data.forEach((ele) => {
+		let div = createEl("div");
+		div.classList.add("movie");
+
+		div.innerHTML = `<img src="${imagUrl + ele.poster_path}" alt="${
+			ele.title
+		}" />
+                <div class="info">
+                    <h3>${ele.title}</h3>
+                    <p><span class="${rateCol(ele.vote_average)}">${
+			ele.vote_average
+		}</span></p>
+                </div>
+				<div class="des"><span style="color:red; font-weight:bold;">Overview: </span> <br/>
+					${ele.overview}
+				</div>`;
+		div.style.cursor = "pointer";
+		div.addEventListener("click", () => {
+			selectObj(ele);
+		});
+
+		box.append(div);
+	});
+}
+
+function rateCol(el) {
+	console.log(el);
+	if (+el <= 3) return "red";
+	else if (+el > 3 && +el <= 7) return "orange";
+	else return "green";
+}
+
+function selectObj(o) {
+	nameMov = o.title;
+	localStorage.setItem("name", nameMov);
+	if (window.location.href === "./index.html") {
+		window.location.href = "./displayPage/display.html";
+	} else {
+		window.location.href = "../displayPage/display.html";
 	}
 }
